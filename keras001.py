fbbf722ae336f45bd8fd37f023f0fe42206d242c
lordyoung1221 <yangkun@cdu.edu.cn>
2019-07-17 23:56:30 +0800

    keras测试

---
+++ keras001.py
@@ -0,0 +1,9 @@
+# _*_ coding: utf-8 _*_
+
+import keras
+from keras.datasets import mnist
+from keras.modals import Sequential
+from keras.layers import Dense,Flatten,Conv2D,MaxPooling2D
+from keras import backend as K
+
+num_classes 
