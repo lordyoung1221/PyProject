43c9fae39e2229c685403bb94c5dd59cb925a55c
lordyoung1221 <yangkun@cdu.edu.cn>
2019-07-18 00:14:37 +0800

    keras测试

--- keras001.py
+++ keras001.py
@@ -1,9 +1,8 @@
 # _*_ coding: utf-8 _*_
 
-import keras
 from keras.datasets import mnist
-from keras.modals import Sequential
-from keras.layers import Dense,Flatten,Conv2D,MaxPooling2D
-from keras import backend as K
 
-num_classes 
+num_classes = 10
+img_rows, img_cols = 28, 28
+
+(trainX, trainY), (testX, testY) = mnist.load_data()
